07e35ec3434cfde59688f22aff1cb0f887f47c46
jacques mouette <j.mouette@gmail.com>
2018-07-26 17:02:08 -0300

    Remove debugging log

--- src/components/toolbar.js
+++ src/components/toolbar.js
@@ -149,7 +149,6 @@ export default class Toolbar extends React.Component {
 
   onChange = (field) => (e) => {
     const value = field === 'coverInputValue' ? e.target.value.toUpperCase().replace(/ /g, '-').replace(/[^\w-]/gi, '') : e.target.value;
-    console.log('onChange value', field, value);
     this.setState({
       [field]: value,
     });

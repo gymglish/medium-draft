df0216818ed1d351b3b913bf71dad5dd02ed32bb
jacques mouette <j.mouette@gmail.com>
2018-08-02 16:48:44 -0300

    Remove unused imports

--- src/components/addplaceholderbutton.js
+++ src/components/addplaceholderbutton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { getVisibleSelectionRect, EditorState, Modifier, Entity } from 'draft-js';
+import { getVisibleSelectionRect } from 'draft-js';
 
 const getRelativeParent = (element) => {
   if (!element) {

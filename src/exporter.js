9fef35d01bc9dadee026f0ffaacc17bc8009b9ff
jacques mouette <j.mouette@gmail.com>
2019-02-05 12:52:05 -0200

    Add missing export definition

--- src/exporter.js
+++ src/exporter.js
@@ -123,6 +123,8 @@ export const blockToHTML = (block) => {
         start: '<div style="text-align: left;">',
         end: '</div>',
       };
+    case Block.CODE:
+      return <code />;
     default: return null;
   }
 };
